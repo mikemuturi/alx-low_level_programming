7e3c9afcf0164701426506d9f1adb04d67ca9567
mikemuturi <mikeirungumuturi@gmail.com>
2023-02-19 08:33:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,14 @@
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a new line."
- * return 0.
+ * return 0 (Success).
  */
 
 int main(void)
 
 {
-	puts("\"Programming is like a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
+
 }
