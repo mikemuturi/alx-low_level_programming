47d03a2f1fd2d8e8c211f58e24f2f85388e9c05a
mikemuturi <mikeirungumuturi@gmail.com>
2023-02-19 09:11:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * man -prints"and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line,to standard error.
- * Return: always 1.
+ * Return: always 1 (Success).
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
