4a0381689ac4314341a50c23513e8429235d2267
MIKE <mikeirungumuturi@gmail.com>
2023-03-14 04:12:13 +0300

    My first commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * str_concat - get ends of input and add together for size
- * @s1: input one to concat
+ * @s1:The first string to concatenate 
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
-	while (s1[i] != '\0')
+	while (s1[i])
 		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-	if (conct == NULL)
+	while (s2[j])
+		j++;
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
 		return (NULL);
-       		i = ci = 0;
-	while (s1[i] != '\0')
-	{	
-	conct[i] = s1[i];
-		i++;
-	}
-	while (s2[ci] != '\0')
+	j = 0;
+	while (k < l)
 	{
-	conct[i] = s2[ci];
-	i++, ci++;
+		if (k <= i)
+			s[k] = s1[k];
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
 	}
-	conct[i] = '\0';
-	return (conct);
+	s[k] = '\0';
+	return (s);
 }
