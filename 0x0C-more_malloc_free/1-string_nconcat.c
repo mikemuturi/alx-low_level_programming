6d6d51246c22210ca0518a67279d99decada883f
MIKE <mikeirungumuturi@gmail.com>
2023-03-16 16:42:27 +0300

    My Commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * string_nconcat - ..
- * @s1: ...
- * @s2: ...
- * @n: ...
- * 
- * Return: ...
+ * _strlen - calculate and return length
+ *
+ * @string: string
+ * Return: string length 
+ */
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+		return (i);
+}
+/**
+ * string_concar - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	char *ptr;
+	int num, len, i, j;
+
+	num = n;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[i])
-		k++;
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
+	if (num < 0)
 		return (NULL);
-	k = 0;
-	while (j < l)
-	{
-		if (j <= i)
-			str[j] = s1[j];
-		if (j >= i)
-		{
-			str[j] = s2[k];
-				k++;
-		}
-		j++;
-	}
-	str[j] = '\0';
-	return (str);
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
+         len = _strlen(s1) + num + 1;
+	 ptr = malloc(sizeof(*ptr) * len);
+	 if (ptr == NULL)
+		 return (NULL);
+	 for (i = 0; s1[i] != '\0'; i++)
+		 ptr[i] = s1[i];
+	 for (j = 0; j < num; j++)
+		 ptr[i + j] = s2[j];
+	 ptr[i + j] = '\0';
+	 return (ptr);
 }
